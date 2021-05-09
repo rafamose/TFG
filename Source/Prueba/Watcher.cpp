1771054a47eeae78a165000de1a5ee3c5b5b920d
rafamose <rafamose6797@gmail.com>
2021-05-10 00:57:30 +0200

    Watcher
    -Watcher mechanic implemented
    -New c++ function

---
+++ Source/Prueba/Watcher.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Math/TransformNonVectorized.h"
+#include "GameFramework/Actor.h"
+#include "Kismet/GameplayStatics.h"
+#include "Watcher.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+
+// Sets default values for this component's properties
+UWatcher::UWatcher()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWatcher::BeginPlay()
+{
+	Super::BeginPlay();
+	Owner = GetOwner();
+
+	MyActor = GetWorld()->GetFirstPlayerController()->GetPawn();
+	// ...
+	
+}
+
+
+// Called every frame
+void UWatcher::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (WatcherTrigger->IsOverlappingActor(MyActor))
+	{
+		APlayerController * Controller = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+		class APlayerController * MyPC = Cast<APlayerController>(Controller);
+		MyPC->ClientSetCameraFade(true, FColor::Black, FVector2D(1.0, 0.0), 10.0);
+		MyActor->SetActorLocation(ActorPosition);
+	}
+
+
+	//
+}
+
---
+++ Source/Prueba/Watcher.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Engine/TriggerVolume.h"
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Watcher.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PRUEBA_API UWatcher : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UWatcher();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* WatcherTrigger;
+
+	AActor* MyActor;
+	AActor* Owner;
+
+	UPROPERTY(EditAnywhere)
+	FVector ActorPosition;
+};
